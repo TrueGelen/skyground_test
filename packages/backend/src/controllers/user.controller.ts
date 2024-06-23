8023853e4e3b4da5a83cc7fd001c4589dec7d1f5
Vlad Filchagin <gelengbagen@gmail.com>
2024-06-23 16:12:36 +0300

    [BE]feat: add handeling cookie and tokens

--- packages/backend/src/controllers/user.controller.ts
+++ packages/backend/src/controllers/user.controller.ts
@@ -4,6 +4,7 @@ import { validate } from "class-validator";
 import { userService } from "../services/user/user.service.js";
 import { CreateUserDto } from "../services/user/dtos/sign-up-user.dto.js";
 import { ApiError } from "../exceptions/api-errors.js";
+import { SignInUserDto } from "../services/user/dtos/sign-in-user.dto.js";
 
 class UserController {
   async signUp(req: Request, res: Response, next: NextFunction) {
@@ -14,18 +15,44 @@ class UserController {
       if (errors.length > 0) {
         return next(ApiError.ValidationError({ errors }));
       }
+
       const user = await userService.signUp(userDto);
+
       res.cookie("refreshToken", user.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
       });
+
       return res.json(user);
     } catch (e) {
-      console.log("UserController > signUp > errors", e);
       return next(e);
     }
   }
-  async signIn(req: Request, res: Response) {}
+  async signIn(
+    req: Request<{ email: string; password: string }>,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const userDto = plainToClass(SignInUserDto, req.body);
+      const errors = await validate(userDto);
+
+      if (errors.length > 0) {
+        return next(ApiError.ValidationError({ errors }));
+      }
+
+      const user = await userService.signIn(userDto);
+
+      res.cookie("refreshToken", user.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+
+      return res.json(user);
+    } catch (e) {
+      return next(e);
+    }
+  }
   async signOut(req: Request, res: Response) {}
   async getUsers(req: Request, res: Response) {
     return res.json(await userService.getUsers());
--- packages/backend/src/services/user/user.service.ts
+++ packages/backend/src/services/user/user.service.ts
@@ -5,6 +5,7 @@ import { ApiError } from "../../exceptions/api-errors.js";
 import { GenerateTokensPayloadDTO } from "../token/dtos/sign-up-user.dto.js";
 import { tokenService } from "../token/token-service.js";
 import { CreateUserDto } from "./dtos/sign-up-user.dto.js";
+import { SignInUserDto } from "./dtos/sign-in-user.dto.js";
 
 class UserService {
   private userRepository = AppDataSource.getRepository(User);
@@ -37,7 +38,41 @@ class UserService {
     return { user: generateTokenPayloadDto, ...tokens };
   }
 
-  async signIn() {}
+  async signIn(userData: SignInUserDto) {
+    const user = await this.userRepository.findOne({
+      where: { email: userData.email },
+    });
+
+    if (user == null) {
+      throw ApiError.ValidationError({
+        message: "There is no user with this email address.",
+        errors: [
+          {
+            property: "email",
+            constraints: {
+              message: "There is no user with this email address.",
+            },
+          },
+        ],
+      });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      userData.password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) {
+      throw ApiError.BadRequest({
+        message: "There is no user with this username and password.",
+      });
+    }
+
+    const generateTokenPayloadDto = new GenerateTokensPayloadDTO(user);
+    const tokens = tokenService.generateTokens({ ...generateTokenPayloadDto });
+
+    return { user: generateTokenPayloadDto, ...tokens };
+  }
 
   async signOut() {}
 
